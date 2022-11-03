3f5ab4e1a5d4879f349643c50ca93f7218bd6745
nmaahs <105306382+mrwindupbirdesq@users.noreply.github.com>
2022-11-03 16:37:55 +0000

    working implementation
    No html, all user interaction through alerts. That said, it works.

--- script.js
+++ script.js
@@ -1,14 +1,102 @@
 function getComputerChoice() {
-  let random = Math.random();
-  if (random > .66) {
-    return "Rock";
+    let random = Math.random();
+    console.log(random);
+    if (random > .66) {
+      return "rock";
+    }
+    else if (random > .33) {
+      return "paper";
+    }
+    else {
+      return "scissors";
+    }
   }
-  else if (random > .33) {
-    return "Paper";
-  }
-  else {
-    return "Scissors";
-  }
+  
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    console.log(playerSelection);
+    console.log(computerSelection);
+    if (playerSelection === 'paper') {
+        if (computerSelection === 'paper'){
+            return tie();
+        }
+        if (computerSelection === 'rock') {
+            return winner('Dumb human');
+        }
+        else {
+            return winner('Rockstar computer');
+        }
+    }
+    else if (playerSelection === 'rock') {
+        if (computerSelection === 'paper'){
+            return winner('Rockstar computer');
+        }
+        if (computerSelection === 'rock') {
+            return tie();
+        }
+        else {
+            return winner('Dumb human');
+        }
+    }
+    else {
+        if (computerSelection === 'scissors') {
+            return tie();
+    }    
+        if (computerSelection === 'rock') {
+            return winner('Rockstar computer');
+        }
+        else {
+            return winner('Dumb human');
+        }
+} }
+
+function winner(string) {
+    alert(`${string} wins!`);
+    return string;
 }
 
-console.log(getComputerChoice());
+function tie() {
+   alert('Tie! Suckers!'); 
+}
+
+function getHumanChoice() {
+    let choice = prompt("Rock - Paper - Scissors? (choose one)").toLowerCase();
+    if (choice === 'rock' || choice === 'paper' || choice === 'scissors') {
+        return choice;
+    } 
+    else {
+        return getHumanChoice();
+    }    
+}
+
+function game() {
+    let computerWins = 0;
+    let humanWins = 0;
+    let playerSelection = getHumanChoice();
+    let roundResult;
+    for (let i = 0; i < 5; i++) {
+        roundResult = playRound(playerSelection, getComputerChoice());
+        if (roundResult === 'Dumb human') {
+            humanWins ++;
+        }
+        else if (roundResult === 'Dumb human') {
+            computerWins ++;
+        }
+        else {
+            //do nothing, tie
+        }
+    }
+    console.log(humanWins);
+    console.log(computerWins);
+    if (humanWins > computerWins) {
+        return alert("final result: dumb human wins");
+    }
+    else if (computerWins > humanWins) {
+        return alert("final result: what did you expect? cpu W");
+    }
+    else {
+        return alert("Nobody wins. How do you like that? Not very fun huh");
+    }
+}
+
+game();
